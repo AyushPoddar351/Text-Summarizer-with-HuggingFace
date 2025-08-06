0276db46bae05eb1923c1025633f15d133a02268
AyushPoddar351 <ayushpoddar351@gmail.com>
2025-08-06 23:32:07 +0530

    Model Evaluation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.TextSummarizer.logging import logger
 from src.TextSummarizer.pipeline.stage_1_data_ingestion import DataIngestionPipeline
 from src.TextSummarizer.pipeline.stage_2_data_transformation import DataTransformationPipeline
 from src.TextSummarizer.pipeline.stage_3_model_trainer import ModelTrainerPipeline
+from src.TextSummarizer.pipeline.stage_4_model_evaluation import ModelEvaluationPipeline
 
 logger.info("Logging setup complete.")
 
@@ -37,3 +38,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationPipeline()
+   model_evaluation.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
